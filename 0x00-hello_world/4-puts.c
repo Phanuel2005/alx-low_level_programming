1261ef84f7242ea46cad86616b9b8c50b8910680
Omamowho Phanuel <omamowhophanuel@gmail.com>
2022-09-12 23:20:29 +0000

    hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
-int main( )
+/**
+ * this function is good
+ * and very nice
+ */
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return(0);
