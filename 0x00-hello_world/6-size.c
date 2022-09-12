af159f0fb83fd0e39fe51f313e5ad23d844265ed
Omamowho Phanuel <omamowhophanuel@gmail.com>
2022-09-12 23:37:17 +0000

    task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+	int i;
+	double d;
+	char c;
+
+	printf("Size of a char: %lu.\n", (unsigned long)sizeof(c));
+	printf("Size of a double: %lu.\n", (unsigned long)sizeof(d));
+	printf("Size of an int: %lu.\n", (unsigned long)sizeof(i));
+
+	return(0)
+}
