cc763f751499230aceb1e62e65b899757fceb794
Omamowho Phanuel <omamowhophanuel@gmail.com>
2022-09-15 04:08:26 +0000

    alx can kill

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ *_islower - checks to see if there is a lowercase in a group of sentence
+ *Return: it returns 1 or 0 which varies with the condition
+ */
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
