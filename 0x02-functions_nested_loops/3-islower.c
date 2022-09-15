67a984a5d78440f42cf3cba4978bcbd0606377a5
Omamowho Phanuel <omamowhophanuel@gmail.com>
2022-09-15 04:10:31 +0000

    kai

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *_islower - checks to see if there is a lowercase in a group of sentence
+ *_islower c  - checks to see if there is a lowercase in a group of sentence
  *Return: it returns 1 or 0 which varies with the condition
  */
 int _islower(int c)
