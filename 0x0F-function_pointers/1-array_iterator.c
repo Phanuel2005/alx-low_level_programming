3dd49afe6162ff1082aa4a246d6d98cbe98e23cc
Omamowho Phanuel <omamowhophanuel@gmail.com>
2023-11-13 17:23:21 +0000

    updated task 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -6,16 +6,17 @@
  *@array : Array function is executed on.
  *@size : size of array.
  *@action : a pointer to function that is used.
+ *Return: void
  */
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	size_t n;
+	unsigned int j;
 
-	n = 0;
-	while (n < size)
+	if (array == NULL || action == NULL)
+		return;
+	for (i = 0; i < size; i++)
 	{
-		action(array[n]);
-		n++;
+		action(array[i]);
 	}
 }
