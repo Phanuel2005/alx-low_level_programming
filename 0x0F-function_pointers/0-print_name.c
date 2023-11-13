fcc1838d2751af7d6e12e6fd02d1b2b38544e20a
Omamowho Phanuel <omamowhophanuel@gmail.com>
2023-11-13 16:51:02 +0000

    updated header file and task 0 file

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -5,9 +5,12 @@
   * print_name- Function that prints name.
   * @name : string that contains name.
   * @f : pointer to function that prints name.
+  * Return: nothing
   */
 
 void print_name(char *name, void (*f)(char *))
 {
+	if (name == NULL || f == NULL)
+		return;
 	f(name);
 }
